3056bce2ea278cfff1255adaf60625f7cd52b70f
hugo <hugo.chantelot@epitech.eu>
2022-04-07 20:32:40 +0200

    [+] add spark [+]

--- Dockerfile
+++ Dockerfile
@@ -8,3 +8,9 @@ RUN pip install pymongo
 RUN pip install nltk
 RUN pip install spacy
 RUN python -m spacy download en_core_web_sm
+RUN pip install sklearn
+RUN pip install matplotlib
+RUN pip install imblearn
+RUN pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu113
+RUN pip install nltk
+RUN pip install spacy
--- work/get_data.py
+++ work/get_data.py
@@ -1,25 +1,24 @@
+#!/usr/bin/python
+
 from pymongo import MongoClient
-import sqlite3
 from sqlite3 import Connection
-import streamlit as st
+import pandas as pd
+import sqlite3
 
 # Collecte des données de notre DataBase
-def get_reviews(client_connect):
-    mydb = client_connect["imdb"]
-    mycol = mydb["imbd_tokenized"]
-    df_db = {
-        "Rate": [],
-        "Comment": []
-    }
-    index = 0
-    for x in mycol.find():
-        df_db["_id"].insert(-1, index)
-        df_db["Rate"].insert(-1, x["Rate"])
-        df_db["Comment"].insert(-1, x["Comment"])
-        index+=1
-    return df_db
+def get_data():
+    client = MongoClient('mongo', port = 27017, username = 'root', password = 'root')
+    db = client.imdb
+    collection = db.imbd_tokenized
+    data = pd.DataFrame(list(collection.find()))
+    return data
 
-if __name__ == "main":
-    client_connect = MongoClient('mongo', port = 27017, username = 'root', password = 'root')
-    df_reviews = get_reviews(client_connect)
-    print(df_reviews)
+def main():
+    data = get_data()
+    df = pd.DataFrame(columns=['Rate', 'Comment'])
+    df['Comment'] = data['Comment']
+    df['Rate'] = data['Rate']
+    return df
+
+if __name__ == "__main__":
+    main()
